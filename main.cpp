84f5422cf9d48ce27bc5971bd5744a037967dfa3
Bilal <danianbilal512@gmail.com>
2026-02-14 23:20:49 +0500

    initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <raylib.h>
+
+using namespace std;
+
+int main () {
+    InitWindow(800, 600, "Hello Raylib!");
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Hello, World!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
